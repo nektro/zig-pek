1f4b7a6d4cb994a8b11185485350d3cbad9b70af
Meghan Denny <hello@nektro.net>
2021-08-13 23:15:36 -0700

    allow `this` in 2nd arg of `#ifequal`

--- src/lib.zig
+++ src/lib.zig
@@ -110,7 +110,7 @@ fn do(writer: anytype, comptime value: astgen.Value, data: anytype, ctx: anytype
                 },
                 .ifequal => {
                     comptime assertEqual(v.args.len, 2);
-                    const y = search(v.args[1], data);
+                    const y = if (comptime std.mem.eql(u8, v.args[1][0], "this")) search(v.args[1][1..], data) else search(v.args[1], ctx);
                     if (x == y) try do(writer, body, data, ctx, indent, flag1);
                 },
                 .ifnotequal => {
