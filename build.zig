6f98a0f349e71b1c54a2e1e8c495a37243b3b722
Meghan Denny <hello@nektro.net>
2024-12-28 19:20:36 -0800

    update to zig 0.13

--- build.zig
+++ build.zig
@@ -1,13 +1,13 @@
 const std = @import("std");
 const deps = @import("./deps.zig");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
 
     const exe = b.addExecutable(.{
         .name = "zig-pek",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = mode,
     });
