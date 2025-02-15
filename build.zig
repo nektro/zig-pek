a4de26641137d522861b4227181c344e9bed412d
Meghan Denny <hello@nektro.net>
2025-02-15 12:46:51 -0800

    add disable_llvm build param

--- build.zig
+++ build.zig
@@ -4,6 +4,7 @@ const deps = @import("./deps.zig");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
+    const disable_llvm = b.option(bool, "disable_llvm", "use the non-llvm zig codegen") orelse false;
 
     const exe = b.addExecutable(.{
         .name = "zig-pek",
@@ -12,6 +13,8 @@ pub fn build(b: *std.Build) void {
         .optimize = mode,
     });
     deps.addAllTo(exe);
+    exe.use_llvm = !disable_llvm;
+    exe.use_lld = !disable_llvm;
     b.installArtifact(exe);
 
     const tests = b.addTest(.{
@@ -20,6 +23,8 @@ pub fn build(b: *std.Build) void {
         .optimize = mode,
     });
     deps.addAllTo(tests);
+    tests.use_llvm = !disable_llvm;
+    tests.use_lld = !disable_llvm;
 
     const test_step = b.step("test", "Run all library tests");
     const tests_run = b.addRunArtifact(tests);
