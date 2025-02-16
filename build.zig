9d95a343c2f9c61b67c7e6f36f0e3907052f140f
Meghan Denny <hello@nektro.net>
2025-02-15 22:04:34 -0800

    remove run step

--- build.zig
+++ build.zig
@@ -6,17 +6,6 @@ pub fn build(b: *std.Build) void {
     const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
     const disable_llvm = b.option(bool, "disable_llvm", "use the non-llvm zig codegen") orelse false;
 
-    const exe = b.addExecutable(.{
-        .name = "zig-pek",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = mode,
-    });
-    deps.addAllTo(exe);
-    exe.use_llvm = !disable_llvm;
-    exe.use_lld = !disable_llvm;
-    b.installArtifact(exe);
-
     const tests = b.addTest(.{
         .root_source_file = b.path("test.zig"),
         .target = target,
